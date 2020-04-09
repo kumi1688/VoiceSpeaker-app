76ffae047f3f83a4e3dbb4d305975dddbcfa3b2f
Ubuntu <ubuntu@ip-172-31-32-102.ap-northeast-2.compute.internal>
2020-04-09 05:03:01 +0000

    [FrontEnd] set base container and check connection between frontend and backend

---
+++ app_frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <h2>현재 상태</h2>
+    <button @click='turnOn'>켜기</button>
+    <button @click='turnOff'>끄기</button>
+  </v-app>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  methods:{
+    setState(value){
+        axios.get(`/api/${value}`).then(() => {
+          this.bulb_state = value;
+        });
+      },
+    turnOn(){
+        console.log('on');
+        this.setState('on');
+    },
+    turnOff(){
+        console.log('off');
+        this.setState('off');
+    }
+  },
+  components: {
+    
+  },
+
+  data(){
+    return {
+      bulb_state : false
+    }
+  }
+};
+</script>
